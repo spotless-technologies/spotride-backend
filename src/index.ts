e4abb88a053e52ed9988e03704a9e8d3db7c6e81
Habeeb <habeebahmadu1@gmail.com>
2026-03-14 16:07:27 +0100

    refactor: build command

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,3 @@
-// import "dotenv/config";
-// import express from 'express';
-// import cookieParser from 'cookie-parser';
-// import swaggerUi from 'swagger-ui-express';
-// import swaggerJsdoc from 'swagger-jsdoc';
-// import authRoutes from './routes/auth.routes';
-// import prisma from './config/prisma';
-
-
-// const app = express();
-// app.use(express.json());
-// app.use(cookieParser());
-
-// const swaggerOptions = {
-//   definition: {
-//     openapi: '3.0.0',
-//     info: { title: 'Robust Auth API', version: '1.0.0' },
-//     servers: [{ url: 'http://localhost:5000' }],
-//   },
-//   apis: ['./src/routes/*.ts', './src/controllers/*.ts'],
-// };
-
-// const swaggerSpec = swaggerJsdoc(swaggerOptions);
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// app.use('/api/auth', authRoutes);
-
-// const PORT = 5000;
-// app.listen(PORT, async () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-//   console.log(`Swagger: http://localhost:${PORT}/api-docs`);
-// });
-
-// app.get('/', (req, res) => {
-//   res.json({ message: "Welcome to SpotRide Auth API", docs: "/api-docs" });
-// });
-
-// process.on('SIGINT', async () => {
-//   await prisma.$disconnect();
-//   process.exit(0);
-// });
-
 import "dotenv/config";
 import express from 'express';
 import cookieParser from 'cookie-parser';
@@ -112,6 +70,8 @@ app.listen(PORT, async () => {
   console.log(`Server running on http://localhost:${PORT}`);
   console.log(`Swagger UI:   http://localhost:${PORT}/api-docs`);
 });
+
+
 
 process.on('SIGINT', async () => {
   console.log('\nShutting down...');
